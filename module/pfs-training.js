34190961ba7c9586b862c342af0ef62ae06a4023
Thomas Love <tll4002@med.cornell.edu>
2023-02-06 21:12:02 -0500

    WIP localization for section label

--- module/pfs-training.js
+++ module/pfs-training.js
@@ -1,13 +1,18 @@
 // early function for rendering the Training Selection
 function renderTrainingSelection() {
-  return '<section class="school"><label class="details-label">Pathfinder Training</label></section>';
+  return '<section class="school"><label class="details-label">' + game.i18n.localize("pfsorgplay-training.display.selection.label") + '</label></section>';
+}
+
+function log(message) {
+  console.log(game.i18n.localize("pfsorgplay-training.logging.prefix") + game.i18n.localize("pfsorgplay-training.logging.delimiter") + game.i18n.localize(message))
 }
 
 // Init Hook
 Hooks.once(
-  'init',
+  "init",
   async () => {
     console.log('PFS Training | Initializing fvtt-pf2e-pforgplay-training');
+    log("pfsorgplay-training.logging.messages.hook.init");
 });
 
 // renderActorSheet Hook
@@ -15,6 +20,7 @@ Hooks.on(
   "renderActorSheet",
   (sheet, html) => {
     console.log("PFS Training | ActorSheet Render Hook");
+    log("pfsorgplay-training.logging.messages.hook.renderActorSheet");
     let $trainingSelection = renderTrainingSelection();
     html.find('.level-bump').after($trainingSelection);
 });
